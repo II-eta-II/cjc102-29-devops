2ac759ba7b349f8d790d1cf98064b05190e60023
eta <yitaoshieh@gmail.com>
2025-12-26 12:47:44 +0800

    reset

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 459063348692.dkr.ecr.ap-northeast-1.amazonaws.com
+- aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 459063348692.dkr.ecr.ap-northeast-1.amazonaws.com
 
 if [ `docker images | grep 459063348692.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-29-ecr-repo | wc -l`  = 1 ]
 then
